bdff2f122f0553a026da9c35c48f0b959627faf4
OrpheusOnMac <pkj.blogger@gmail.com>
2021-09-11 20:45:31 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/* Public Routes */
+Route::get('/test', function (Request $request) {
+    return json_encode($request, true);
+});
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+/* Protected Routes */
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('/secure/test', function (Request $request) {
+        return response(["user" => $request->user()], 200);
+    });
 });
