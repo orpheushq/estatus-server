6fb4eee6445dc3dd7cc2a0e2f24e8177745e1891
OrpheusOnMac <pkj.blogger@gmail.com>
2021-09-21 21:20:32 +0530

    Update PatientSeeder.php

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -18,8 +18,56 @@ class PatientSeeder extends Seeder
             'patientNo' => 1,
             'name' => 'Anne Smith',
             'gender' => 'Female',
-            'dob' => "1997-01-01",
+            'dob' => "1960-01-01",
             'diabetesType'=>'Type 1'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 2,
+            'name' => 'John Doe',
+            'gender' => 'Male',
+            'dob' => "1997-03-12",
+            'diabetesType'=>'Type 2'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 3,
+            'name' => 'Jake Fauci',
+            'gender' => 'Male',
+            'dob' => "1989-02-09",
+            'diabetesType'=>'Gestational'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 44,
+            'name' => 'Frank Simmons',
+            'gender' => 'Male',
+            'dob' => "1993-09-20",
+            'diabetesType'=>'Type 2'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 98,
+            'name' => 'Niel Harris',
+            'gender' => 'Male',
+            'dob' => "1996-09-20",
+            'diabetesType'=>'Gestational'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 983,
+            'name' => 'Jennifer Miel',
+            'gender' => 'Female',
+            'dob' => "2010-12-22",
+            'diabetesType'=>'Pre-diabetes'
+        ]);
+
+        DB::table('patients')->insert([
+            'patientNo' => 1983,
+            'name' => 'Tani Myers',
+            'gender' => 'Female',
+            'dob' => "2007-12-22",
+            'diabetesType'=>'Pre-diabetes'
         ]);
     }
 }
