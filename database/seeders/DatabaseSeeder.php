3b5d1e3180266c1f32bfa9a46135a303c99f0341
Orpheus on Mac <admin@orpheus.digital>
2021-11-02 00:29:40 +0530

    Changed to reflect permissions and new dummy names

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //super-admin
         DB::table('users')->insert([
             'name' => "Orpheus Digital",
             'email' => 'admin@orpheus.digital',
             'password' => Hash::make('decrease'),
         ]);
         DB::table('users')->insert([
-            'name' => "Jane Doe",
-            'email' => 'jane@example.com',
-            'password' => Hash::make('jane123'),
+            'name' => "admin",
+            'email' => 'admin@lorem.dev',
+            'password' => Hash::make('admin123'),
         ]);
+
+        //hospital-admin
         DB::table('users')->insert([
-            'name' => "Ben James",
-            'email' => 'ben@acme.com',
+            'name' => "Anne Bennett",
+            'email' => 'anne@lorem.dev',
+            'password' => Hash::make('anne123'),
+        ]);
+
+        //professional
+        DB::table('users')->insert([
+            'name' => "Ben Chrisworth",
+            'email' => 'ben@lorem.dev',
             'password' => Hash::make('ben123'),
         ]);
     }
