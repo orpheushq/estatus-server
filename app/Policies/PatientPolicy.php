03cec3d4dca9a1bb55c8bf75a68e9815ad07c93f
Orpheus on Mac <admin@orpheus.digital>
2021-11-03 01:34:45 +0530

    Added patient policy

---
+++ app/Policies/PatientPolicy.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Patient;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PatientPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Patient $patient)
+    {
+        //
+        if ($user['orgnaizationId'] !== $patient['organizationId'] && $user->hasPermissionTo('external patients')) {
+            return true;
+        }
+        if ($user['orgnaizationId'] === $patient['organizationId'] && $user->hasPermissionTo('internal patients')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Patient $patient)
+    {
+        //
+        if ($user->organizationId !== $patient->organizationId && $user->hasPermissionTo('external patients')) {
+            return true;
+        }
+        if ($user->organizationId === $patient->organizationId && $user->hasPermissionTo('internal patients')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Patient $patient)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Patient $patient)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Patient $patient)
+    {
+        //
+    }
+}
