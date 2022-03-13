5e7caf53192f6719b905828a9de05983ee370fe7
OrpheusOnApollo <3007446+praveenjayakody@users.noreply.github.com>
2022-03-13 21:21:02 +0530

    Added branding accents to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,12 @@
 @extends('adminlte::auth.login')
+
+@section('auth_footer')
+    @parent
+    <p class="my-0 orpheus-brand">
+        <a href="https://orpheus.digital/">Solved by Orpheus Digital</a>
+    </p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/login_custom.css') }}">
+@stop
