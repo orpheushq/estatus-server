75913f69d4a79bb18757fd31059fab78cb17b249
OrpheusOnApollo <3007446+praveenjayakody@users.noreply.github.com>
2022-03-19 14:59:13 +0530

    Added default password validation rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Validation\Rules\Password;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Password::defaults(function () {
+            $rule = Password::min(8);
+     
+            return $rule;
+        });
     }
 }
