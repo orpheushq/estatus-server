18b4e11436dbe7eea95260df725c37219c86133e
OrpheusOnApollo <3007446+praveenjayakody@users.noreply.github.com>
2022-03-22 20:37:27 +0530

    Added new permission

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -23,11 +23,16 @@ class RolesAndPermissionsSeeder extends Seeder
         Permission::create(['name' => 'internal users']); // users of the same organization as the signed in user
         Permission::create(['name' => 'external users']); // users outside of the signed in user's organization
 
+        Permission::create(['name' => 'add locations']); // users with this permission can add new locations
+
         // create a role and assign an array of permissions
         $role = Role::create(['name' => 'branch-admin'])
             ->givePermissionTo(['internal users']);
 
         $role = Role::create(['name' => 'client']);
+
+        $role = Role::create(['name' => 'guide'])
+            ->givePermissionTo(['add locations']);
 
         $role = Role::create(['name' => 'super-admin'])
             ->givePermissionTo(Permission::all());
