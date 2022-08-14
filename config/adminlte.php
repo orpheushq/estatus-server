3345cb78deb6fc3712181a6af8853ca91ce41f78
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2022-08-14 23:01:31 +0530

    Changed branding

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'Admin Panel',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Bisson</b>DMS',
+    'logo' => '<b>Admin</b>Panel',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'BissonDMS',
+    'logo_img_alt' => 'Admin Panel',
 
     /*
     |--------------------------------------------------------------------------
