afcf0395cf8a906250ee9e3ae357df0a0af04fd5
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2023-02-05 18:22:24 +0530

    Added code to process rentals

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -9,6 +9,8 @@ class Rental extends Model
 {
     use HasFactory;
 
+    protected $guarded = []; //make all attributes mass assignable
+
     public function property()
     {
         return $this->morphOne(Property::class, 'propertyable');
