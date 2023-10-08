9f6c89d821dfe5852d866df99d52d402ae5ed6d1
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2023-10-09 01:54:57 +0530

    Update 2023_09_23_191226_create_regions_table.php

--- database/migrations/2023_09_23_191226_create_regions_table.php
+++ database/migrations/2023_09_23_191226_create_regions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('regions', function (Blueprint $table) {
             $table->id();
-            $table->string('region'); 
+            $table->string('region');
             $table->timestamps();
         });
     }
