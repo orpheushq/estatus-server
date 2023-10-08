ee466ca20114673dc0eaeda60509ec04cf73e38d
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2023-10-09 02:10:08 +0530

    Update 2023_09_23_191819_create_region_statistics_table.php

--- database/migrations/2023_09_23_191819_create_region_statistics_table.php
+++ database/migrations/2023_09_23_191819_create_region_statistics_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('region_statistics', function (Blueprint $table) {
             $table->id();
-            $table->integer('region_id'); 
+            $table->integer('region_id');
             $table->integer('price');
             $table->timestamps();
         });
