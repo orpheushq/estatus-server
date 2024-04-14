418310b5ead9b9e8c33c1e65045138ecf56e4f6c
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2024-04-14 20:40:12 +0530

    Refinement

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -27,7 +27,14 @@ class PropertyController extends Controller
     {
         $region = $request->get('region');
         $properties = Property::where('area', '=', $region)->select('address')->distinct()->get();
-        return response(array_map(fn($property): string => $property['address'], $properties->toArray()));
+        return response(
+            array_values(
+                array_filter(
+                    array_map(fn($property): string | null => $property['address'], $properties->toArray()),
+                    fn($address): string => !is_null($address)
+                )
+            )
+        );
     }
 
     private function updateEntity($newValues, $entity)
