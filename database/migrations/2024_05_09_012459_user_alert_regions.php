03c72c8255514bcf162d31b72c9de11cb6e5edff
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2024-05-21 08:42:28 +0530

    Column properties

--- database/migrations/2024_05_09_012459_user_alert_regions.php
+++ database/migrations/2024_05_09_012459_user_alert_regions.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('alert_regions');
+            $table->json('alert_regions')->nullable()->default(NULL);
         });
     }
 
