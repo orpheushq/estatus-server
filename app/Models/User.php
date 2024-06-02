610d7eb930654769ad577cf6e82bbf3ac3e7feef
PraveenOnMac <3007446+praveenjayakody@users.noreply.github.com>
2024-06-02 12:24:09 +0530

    Alert range update and viewing

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,6 +48,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'alert_range' => AsArrayObject::class,
+        'alert_regions' => AsArrayObject::class,
     ];
 
     /**
